bda67d3ccd6b1604727910e106c174f80ae96a2b
Raghavendra Mirajkar <raghavendramirajkar@Raghavendras-MacBook-Air.local>
2024-03-13 19:02:53 +0530

    commit

--- Extensions with Protocols.playground/Contents.swift
+++ Extensions with Protocols.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//extension Double {
+//    func round(to places : Int) -> Double {
+//        let precisionNumber = pow(10, Double(places))
+//        var n = self
+//        n = n * (precisionNumber)
+//        n.round()
+//        n = n / precisionNumber
+//        return n
+//    }
+//}
+//
+//var myDouble = 3.14159
+//
+//myDouble = myDouble * 1000
+//myDouble.round()
+//myDouble = myDouble / 1000
+//
+//myDouble.round(to : 2)
+
+let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+button.backgroundColor = .red
+extension UIButton {
+    func makeCircular() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = self.frame.size.width / 2
+        self.backgroundColor = .red
+    }
+}
+
+let button1 = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+button1.makeCircular()
+
